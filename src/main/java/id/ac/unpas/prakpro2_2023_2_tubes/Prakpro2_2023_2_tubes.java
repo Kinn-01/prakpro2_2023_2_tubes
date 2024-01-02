68cfc4cac54a2bb83e44f038399bfbbc887b08b4
Kinn-01 <gilmanarief23@gmail.com>
2024-01-02 15:00:02 +0700

    Membuat Java Maven Project

---
+++ src/main/java/id/ac/unpas/prakpro2_2023_2_tubes/Prakpro2_2023_2_tubes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package id.ac.unpas.prakpro2_2023_2_tubes;
+
+/**
+ *
+ * @author Gilman Arief
+ */
+public class Prakpro2_2023_2_tubes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
